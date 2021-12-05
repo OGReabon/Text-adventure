24e6e7c202ea0d29fe0082ad8825fa9321d4f9a0
Mato098 <43574821+Mato098@users.noreply.github.com>
2021-12-05 23:34:46 +0100

    add basic shit

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from typing import List, Optional
+
+
+class Events:
+    def __init__(self, text: str, choices: List[str], next_events: Optional[List['Events']]):
+        self.text = text
+        self.choices = choices
+        self.next_events = next_events
+
+    def play(self):
+        print("* ----------------- *")
+        print(self.text)
+        print()
+        print(f"Choices: ", end="")
+        for i in self.choices:
+            print(i, end=" ")
+        print()
+        choice = "not a choice 8379vngu2rf255"
+
+        if not self.choices:
+            input("press [enter] to exit")
+            sys.exit()
+
+        while choice not in self.choices:
+            choice = input("Your choice: ")
+        next = self.choices.index(choice)
+        self.next_events[next].play()
+
+
+example_story = Events("prvy event", ["moznost1", "moznost2"],
+                       [Events("druhy event", [], None)])
+
+
+example_story.play()
