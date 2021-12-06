d891ba3169e6bae1b435bb5adc636ebca8f6d93d
Martin Macko <martinmacko098@gmail.com>
2021-12-06 10:46:38 +0100

    changees

--- main.py
+++ main.py
@@ -2,35 +2,39 @@ import sys
 from typing import List, Optional
 
 
-class Events:
-    def __init__(self, text: str, choices: List[str], next_events: Optional[List['Events']]):
+class Event:
+    def __init__(self, text: List[str], choices: List[str], next_events: Optional[List['Event']]):
         self.text = text
         self.choices = choices
         self.next_events = next_events
 
     def play(self):
         print("* ----------------- *")
-        print(self.text)
+        for line in self.text:
+            print(line)
         print()
+
         print(f"Choices: ", end="")
         for i in self.choices:
             print(i, end=" ")
         print()
-        choice = "not a choice 8379vngu2drf255"
 
         if not self.choices:
             input("press [enter] to exit")
             sys.exit()
 
+        choice = "not a choice 8379vngu2drf255"
         while choice not in self.choices:
             choice = input("Your choice: ")
         next = self.choices.index(choice)
         self.next_events[next].play()
 
 
-example_story = Events("prvy event", ["moznost1", "moznost2"],
-                       [Events("druhy event 1", [], None),
-                        Events("druhy event 2", [], None)])
+event_death = Event(["YOU HAVE DIED"], [], None)
+
+example_story = Event(["prvy event", "text k tomu", "dalsi text k tomu"], ["moznost1", "moznost2"],
+                      [Event(["druhy event 1"], [], None),
+                       event_death])
 
 
 example_story.play()
